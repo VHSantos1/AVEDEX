e9affac64d2b2c539e2b09ed79b00c768c054cf0
izadrt <lcristhyne21@gmail.com>
2026-07-14 19:58:23 -0300

    Atualiza avedex.py

--- avedex.py
+++ avedex.py
@@ -1,3 +1,55 @@
+print("=" * 40)
+print(" AVEDEX")
+print("=" * 40)
+
+nome_usuario = input("Digite seu nome: ").strip()
+opcao_menu = ""
+
+while opcao_menu != "0":
+    print()
+    print("=" * 40)
+
+    print("MENU PRINCIPAL")
+    print("=" * 40)
+    print("1 - Ver mensagem de boas-vindas")
+    print("2 - Conhecer uma ave")
+    print("3 - Ver uma curiosidade sobre aves")
+    print("4 - Sobre a AveDex")
+    print("0 - Sair")
+
+    opcao_menu = input("Escolha uma opção: ").strip()
+    print()
+
+    if opcao_menu == "1":
+        print(f"Olá, {nome_usuario}!")
+        print("Seja bem-vindo(a) à AveDex.")
+        print("Aqui vamos conhecer aves e praticar")
+
+    elif opcao_menu == "2":
+        print("Ave escolhida: Bem-te-vi")
+        print("Nome científico: Pitangus sulphuratus"
+        print("O bem-te-vi é uma das aves mais conhec")
+
+    elif opcao_menu == "3":
+        print("Curiosidade:")
+        print("Muitas aves ajudam no equilíbrio ambie")
+
+    elif opcao_menu == "4":
+        print("Sobre a AveDex:")
+        print("A AveDex será um catálogo interativo")
+        print("Ao longo da disciplina, adicionaremos")
+
+    elif opcao_menu == "0":
+        print("Encerrando a AveDex.")
+        print(f"Até logo, {nome_usuario}!")
+
+    else:
+        print("Opção inválida. Digite apenas 0, 1, 2,")
+
+    if opcao_menu != "0":
+        input("\nPressione ENTER para voltar ao menu.")
+
+
 def exibir_linha():
     print("=" * 40)
 
@@ -17,64 +69,87 @@ def exibir_menu():
 def mostrar_boas_vindas(nome_usuario):
     print(f"Olá, {nome_usuario}!")
     print("Seja bem-vindo(a) à AveDex.")
-    print("Aqui vamos conhecer aves e praticar boas práticas.")
+    print("Aqui vamos conhecer aves e praticar boas p")
 
 
 def mostrar_ave_inicial():
     print("Ave escolhida: Bem-te-vi")
     print("Nome científico: Pitangus sulphuratus")
-    print("O bem-te-vi é uma das aves mais conhecidas do Brasil.")
+    print("O bem-te-vi é uma das aves mais conhecidas")
 
 
 def mostrar_curiosidade():
     print("Curiosidade:")
-    print("Muitas aves ajudam no equilíbrio ambiental ao dispersar sementes.")
+    print("Muitas aves ajudam no equilíbrio ambiental")
 
 
 def mostrar_sobre():
     print("Sobre a AveDex:")
-    print("A AveDex será um catálogo interativo de aves.")
-    print("Ao longo da disciplina, adicionaremos novas funcionalidades.")
+    print("A AveDex será um catálogo interativo de av")
 
 
 def pausar():
     input("\nPressione ENTER para voltar ao menu...")
 
 
-# Programa principal
-exibir_linha()
-print("AVEDEX")
-exibir_linha()
+if opcao_menu == "1":
+    mostrar_boas_vindas(nome_usuario)
+elif opcao_menu == "2":
+    mostrar_ave_inicial()
+elif opcao_menu == "3":
+    mostrar_curiosidade()
+elif opcao_menu == "4":
+    mostrar_sobre()
 
-nome_usuario = input("Digite seu nome: ").strip()
-opcao_menu = ""
 
-while opcao_menu != "0":
-    exibir_menu()
-    opcao_menu = input("Escolha uma opção: ").strip()
-    print()
 
-    if opcao_menu == "1":
-        mostrar_boas_vindas(nome_usuario)
 
-    elif opcao_menu == "2":
-        mostrar_ave_inicial()
 
-    elif opcao_menu == "3":
-        mostrar_curiosidade()
 
-    elif opcao_menu == "4":
-        mostrar_sobre()
 
-    elif opcao_menu == "0":
-        print("Encerrando a AveDex.")
-        print(f"Até logo, {nome_usuario}!")
 
-    else:
-        print("Opção inválida. Digite apenas 0, 1, 2, 3 ou 4.")
 
-    if opcao_menu != "0":
-        pausar()
+
+
+
+
+
+
+
+
+
+
+
+
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
