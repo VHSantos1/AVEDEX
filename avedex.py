46fc83272db638f5802afae408620d4958339fe5
izadrt <lcristhyne21@gmail.com>
2026-07-14 20:17:09 -0300

    Organiza aves em catálogo e adiciona listagem

--- avedex.py
+++ avedex.py
@@ -12,7 +12,7 @@ while opcao_menu != "0":
     print("MENU PRINCIPAL")
     print("=" * 40)
     print("1 - Ver mensagem de boas-vindas")
-    print("2 - Conhecer uma ave")
+    print("2 - Listar aves")
     print("3 - Ver uma curiosidade sobre aves")
     print("4 - Sobre a AveDex")
     print("0 - Sair")
@@ -60,7 +60,7 @@ def exibir_menu():
     print("MENU PRINCIPAL")
     exibir_linha()
     print("1 - Ver mensagem de boas-vindas")
-    print("2 - Conhecer uma ave")
+    print("2 - Listar aves")
     print("3 - Ver uma curiosidade sobre aves")
     print("4 - Sobre a AveDex")
     print("0 - Sair")
@@ -72,10 +72,14 @@ def mostrar_boas_vindas(nome_usuario):
     print("Aqui vamos conhecer aves e praticar boas p")
 
 
-def mostrar_ave_inicial():
-    print("Ave escolhida: Bem-te-vi")
-    print("Nome científico: Pitangus sulphuratus")
-    print("O bem-te-vi é uma das aves mais conhecidas")
+def listar_aves(catalogo):
+    print()
+    exibir_linha()
+    print("AVES CADASTRADAS")
+    exibir_linha()
+
+    for ave in catalogo:
+        print(f"{ave['codigo']} - {ave['nome_popular']}")
 
 
 def mostrar_curiosidade():
@@ -95,11 +99,145 @@ def pausar():
 if opcao_menu == "1":
     mostrar_boas_vindas(nome_usuario)
 elif opcao_menu == "2":
-    mostrar_ave_inicial()
+    listar_aves(catalogo_aves)
 elif opcao_menu == "3":
     mostrar_curiosidade()
 elif opcao_menu == "4":
     mostrar_sobre()
+
+
+catalogo_aves = [
+    {
+        "codigo": "1",
+        "nome_popular": "Bem-te-vi",
+        "nome_cientifico": "Pitangus sulphuratus",
+        "habitat": "Áreas abertas, cidades e bordas de florestas",
+        "alimentacao": "Insetos, frutos e pequenos animais",
+        "curiosidade": "Seu canto lembra a expressão bem-te-vi."
+    },
+    {
+        "codigo": "2",
+        "nome_popular": "Canário-da-terra",
+        "nome_cientifico": "Sicalis flaveola",
+        "habitat": "Campos, áreas abertas e ambientes rurais",
+        "alimentacao": "Sementes e pequenos insetos",
+        "curiosidade": "O macho possui plumagem amarela intensa."
+    },
+    {
+        "codigo": "3",
+        "nome_popular": "João-de-barro",
+        "nome_cientifico": "Furnarius rufus",
+        "habitat": "Campos, cidades e áreas rurais",
+        "alimentacao": "Insetos e outros pequenos invertebrados",
+        "curiosidade": "Constrói um ninho de barro característico."
+    }
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
